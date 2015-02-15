4a51f98c08a802b97c27486ee8e0c19e1fa2c6d5
James Croft <james@gomake.com.au>
2015-02-15 17:05:03 +1000

    copy from FA

--- assets/js/fontawesome-cheatsheet.js
+++ assets/js/fontawesome-cheatsheet.js
@@ -1,11 +1,13 @@
 /* Bootstrap Cheat Sheets
    
-   Glyphicon Cheat Sheet
+   Font Awesome Cheat Sheets
    
    By: James Croft (@jamescroft on Twitter)
    
    Contribute or fork this project.  
  */
+
+
 
 $(window).load(function() {
   // When the page has loaded, reveal
@@ -13,13 +15,14 @@ $(window).load(function() {
   $("#about").css("opacity", 1 );
 });
 
+
 $(document).ready(function (){
-	
-// Animated scroll to page anchors via nav
+
+	// Animated scroll to page anchors via nav
 
 	var fixedNavHeight = $(".navbar").height();
-
-function scrollToAbout() {
+	
+$("#scroll-to-about").click(function() {
 	var scrollPos = $('#about').offset().top - (fixedNavHeight - 1);
 		$('html, body').animate({
 			scrollTop: scrollPos
@@ -28,10 +31,10 @@ function scrollToAbout() {
 				$(".navbar-toggle").click();
 			};
     	});
-}
+});
 
-function scrollToHome() {
-		var scrollPos = $('#home').offset().top - (fixedNavHeight - 1);
+$("#scroll-to-home").click(function() {
+	var scrollPos = $('#home').offset().top - (fixedNavHeight - 1);
 		$('html, body').animate({
 			scrollTop: scrollPos,
 		}, 1000, "easeInOutExpo", function () {
@@ -39,56 +42,49 @@ function scrollToHome() {
 				$(".navbar-toggle").click();
 			};
     	});
-
-}
-
-	
-	$("#scroll-to-about").click(function() {
-		scrollToAbout();
-	});
-	
-	$("#scroll-to-home").click(function() {
-		scrollToHome();
-	});
-
-//Create rows: 10 icons per row
-
-function createGlyphRows() {
-	var glyphCount = $("li.grid-icon").length;	
-	var rowSize = 10;
-	for (var c = 0; c<=glyphCount;c+=20) {
-		$("li.grid-icon").slice(c, c+rowSize).wrapAll("<div class='row'><div class='container'></div></div>");
-	};
-	for (var c = 10; c<=glyphCount;c+=20) {
-		$("li.grid-icon").slice(c, c+rowSize).wrapAll("<div class='row row-alt'><div class='container'></div></div>");
-	};
-	$("li.grid-icon .btn-container").attr("display","block");
-	$("ul.glyphicons .row:first").attr("id", "first-row");
-	$("ul.glyphicons").css("max-width", "100%");
-	
-};
-
-createGlyphRows();
-	
-/* Search: https://github.com/DeuxHuitHuit/quicksearch */
-// The search function needs to be timedOut so the classnames can be loaded in first
+});
 
 setTimeout(function() {
+    
+//Create rows: 10 icons per row
+
+	var glyphCount = $("li.grid-icon").length;	
+	var rowSize = 10;
+	for (var c = 0; c <= glyphCount; c += 20) {
+		$("li.grid-icon").slice(c, c + rowSize).wrapAll("<div class='row'><div class='container'></div></div>");
+	};
+	for (var c = 10; c <= glyphCount; c += 20) {
+		$("li.grid-icon").slice(c, c + rowSize).wrapAll("<div class='row row-alt'><div class='container'></div></div>");
+	};
+	$("ul.font-awesome-icons .row:first").attr("id", "first-row");
+    
+//Add 2 Adsense banners    
+
+    $("ul.font-awesome-icons .row:nth-child(3)").attr("id", "third-row");
+    $("<div class='container-wide text-center ad-break'><script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script><!-- Font-Awesome-2nd --><ins class='adsbygoogle'style='display:inline-block;width:728px;height:90px'data-ad-client='ca-pub-8168184751026221'data-ad-slot='4858614992'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>").insertAfter("#third-row");                           
+    $("ul.font-awesome-icons .row:nth-child(6)").attr("id", "sixth-row");
+    $("<div class='container-wide text-center ad-break'><script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script><!-- Font Awesome-3rd --><ins class='adsbygoogle'style='display:inline-block;width:728px;height:90px'data-ad-client='ca-pub-8168184751026221'data-ad-slot='7532879794'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});  </script></div>").insertAfter("#sixth-row");
+    
+	$("ul.font-awesome-icons").css("max-width", "100%");
+
+ //Search: https://github.com/DeuxHuitHuit/quicksearch 
+ //The search function needs to be timedOut so the classnames can be loaded in first
+
 	$('input#glyph-search').quicksearch('li.grid-icon', {
 		 'onBefore': function () {
-        $('ul.glyphicons div.row').hide();
+        $('ul.font-awesome-icons div.row').hide();
         $('div.ad-break').hide();
-		$('li.grid-icon').appendTo('#first-row');
+		$('li.grid-icon').addClass('temp').appendTo('#first-row');
     },
 		'show': function () {
 			$(this).show();
 			$(this).parent('div.row').show();
 		},
-		'onAfter': function () {	
+		'onAfter': function () {
 			window.scrollTo(0, 0);
-    	}
+   		}
 		});
-}, 100);
+}, 1000);
 
 //Supress Enter key
 
@@ -107,54 +103,33 @@ if($('#glyph-search').length) {
 		$('#glyph-search').val('').trigger('keyup').focus();
 	});
 };
-		
-		
+			
  //End Search
-	
-	//For each of the icons in the grid 
- 
-	$(".grid-icon span.glyphicon").each(function(index){		
-	//Find the unicode reference in the data-unicode attribute and render it as HTML inside the <i> tag		
-		glyphStore = $(this);
-		if ($(this).attr("data-unicode")) {
-				$(glyphStore).html("&#x" + $(glyphStore).attr("data-unicode") + ";");
-		};		
-	//Finds the class of the font awesome icon in the grid and display it underneath		
-		var classCode = glyphStore.attr("class").split(" ");
-		 $(glyphStore).next(".glyphicon-class").text("." + classCode[1]);		 
-	});
-	
-	//Adds a break to the glyphicon class prefix, helps with display of class names in grid
-	 $(".glyphicon-class").each(function() {
-		var html = $(this).html().split("-");
-		html = html[0] + "-" + "<br>" + html.slice(1).join("-");
-		$(this).html(html);
-	 });
 
- 		
-//End of element insertions into page, so initialise scrollspy
+//Initialise scrollspy
 
 $('body').scrollspy({ target: '.navbar', offset: (fixedNavHeight - 1)    });
 
 if (Modernizr.touch) {
-	 $("ul.glyphicons li.grid-icon .btn-container").hide();
+	 $("ul.font-awesome-icons li.grid-icon .btn-container").hide();
 };
 
 }); //End document ready
 
+// Using ZeroClipboard to copy glyph codes to clipboard. This is set in a Timeout to allow the Mustache-templated items to be loaded in.
 
-// Using ZeroClipboard to copy glyph codes to clipboard.
-
-var client = new ZeroClipboard( $('.copy-button'), {
+setTimeout (function(){
+   var client = new ZeroClipboard( $('.copy-button'), {
   moviePath: "assets/zeroclipboard-1.2.3/ZeroClipboard.swf"
-} );
+});
+
 
 client.on( "load", function(client, args) {
 
 	client.on( 'mouseover', function ( client, args ) {
 	  //Sets the value to be injected into the clipboard as the item on mouseover
 	  	
-	  glyphStore = $(this).parents("li.grid-icon").children("span.glyphicon");
+	  glyphStore = $(this).parents("li.grid-icon").children("i.fa");
 	  if ($(this).hasClass("parent-copy")) {
 		  //Copy the HTML Tag into the clipboard
 		  //Because of the unicode symbols rendered as contents in the <i> tag, a local clone must be taken and emptied to parse the correct clipboard value
@@ -222,7 +197,9 @@ client.on( 'noflash', function ( client, args ) {
 } );	
 client.on( 'wrongflash', function ( client, args ) {
   alert("Your flash is too old " + args.flashVersion);
-} );
+} ); 
+
+},600);
 
 
 
