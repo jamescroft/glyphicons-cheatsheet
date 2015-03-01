ae5caccc68db0723c16272ba943674050bd5e932
James Croft <james@gomake.com.au>
2015-03-01 22:50:57 +1000

    all 3 copy buttons now working
    added copyable icons, fixed Zeroclipboard button fails, added basic
    formatting, deleted extraneous files and tidied up project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React Rebuild
 
-This is an experiment in which I'm trying to recreate the original in React.js. It's not quite finished yet.
+This is an experiment in which I'm trying to recreate the original Bootstrap Cheatsheets in React.js. It's not quite finished yet.
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,25 @@
  var Icon = React.createClass({
           render: function() {
-          var classString = 'glyphicon ';
+          var classString = 'glyphicon glyphicon-';
               if (this.props.class) {
-                classString += this.props.class;
+                classString += this.props.class + ' display-icon';
               }
           var classHTMLcopy = "<span class='"+ classString + "'></span>"; 
-          var classUnicodeEntity = "&#x" + this.props.class;
+          var classUnicodeEntity = "\u0026" + "\u0023" + "x" + this.props.unicode + ";";
+          var classCssRule = "content:\"\\" + this.props.unicode + "\";";
         
             return (
-              <div className="icon text-center col-xs-12">
-                <span className={classString}></span>
-                <p><code>.{this.props.class}</code></p>
+              <div className="icon col-lg-1 col-md-2 col-sm-3 col-xs-4">
+                <span className={classString} dangerouslySetInnerHTML={{__html: classUnicodeEntity }}></span>
+                <p className="icon-name"><code>{this.props.class}</code></p>
                 <div className="btn-group">
-                <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                <button type="button" className="btn btn-default dropdown-toggle btn-xs" data-toggle="dropdown" aria-expanded="false">
                 Copy <span className="caret"></span>
                 </button>
                 <ul className="dropdown-menu" role="menu">
-                <li><a className="copy-button" href="#" data-clipboard-text={classHTMLcopy}>HTML Tag</a></li>
-                <li><a className="copy-button" href="#" data-clipboard-text={classUnicodeEntity}>HTML Entity</a></li>
-                <li><a href="#">CSS Rule</a></li>
+                <li><a className="copy-button" href="#" data-clipboard-text={classHTMLcopy}><span className="glyphicon glyphicon-copy"></span> HTML Tag</a></li>
+                <li><a className="copy-button" href="#" data-clipboard-text={classUnicodeEntity}><span className="glyphicon glyphicon-screenshot"></span> HTML Entity</a></li>
+                <li><a className="copy-button" href="#" data-clipboard-text={classCssRule}><span className="glyphicon glyphicon-globe"></span> CSS Rule</a></li>
                 </ul>
                 </div>
                 <hr />
@@ -31,7 +32,7 @@
           render: function() {         
             var iconNodes = this.props.data.map(function (icon) {    
               return (
-                <Icon class={icon.id}>
+                <Icon class={icon.id} unicode={icon.unicode}>
                   {icon.id}
                 </Icon>
               );
@@ -43,8 +44,7 @@
             );
           }
         });
-
-              
+             
         var Cheatsheet = React.createClass({        
             loadCommentsFromServer: function() {
                 $.ajax({
@@ -69,7 +69,7 @@
               render: function() {
                 return (
                   <div className="col-md-12">
-                    <h1 className="text-center">Glyphicons Cheatsheet</h1>
+                    <h1 className="text-center">Bootstrap Glyphicons v3.2.2</h1>
                     <IconList data={this.state.data} />     
                   </div>
                 );
