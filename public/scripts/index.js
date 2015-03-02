498bbd1b7a7d6015ba4fcbc890c3fe56a2c6be7f
James Croft <james@gomake.com.au>
2015-03-02 21:52:15 +1000

    added ads, added nav, started 'about'

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -44,6 +44,71 @@
             );
           }
         });
+
+        var NavBarCheat = React.createClass({
+          render: function() {         
+            return (
+              <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+               <div className="container nav-contain">
+                <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span> </button>
+                <a className="navbar-brand" href="http://bootstrapcheatsheets.com"><img src="images/logo-white-h58@2x.png" alt="Bootstrap Cheat Sheets logo" width="144" height="58" /></a>
+                </div>
+                <div className="collapse navbar-collapse">
+                <ul className="nav navbar-nav pull-right">
+                    <li className="nav-search">
+                        <div className="input-group">
+                        <input id="glyph-search" type="text" className="form-control" placeholder="Search" />
+                        <span className="input-group-btn">
+                          <button className="btn btn-default" type="button" id="filter-clear">X</button>
+                        </span>
+                        </div>
+                    </li>
+                    <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="1000" data-close-others="false">Digital <b className="caret"></b></a>
+                    <ul className="dropdown-menu">
+                    <li><a href="#home" id="scroll-to-home">Glyphicons</a></li>
+                    <li><a href="http://fontawesome.bootstrapcheatsheets.com">Font Awesome</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="#about" id="scroll-to-about">About</a></li>
+                    <li><a href="http://bootstrapcheatsheets.com/shop">Shop</a></li>
+                </ul>
+                </div>
+                </div>
+              </nav>
+            );
+          }
+        }); 
+
+        var AboutCheatsheet = React.createClass({
+            render: function() {
+             return (
+                 <div className="about" id="about">
+                                     <div className="container">
+                        <div className="col-md-8">
+                            <h1>How to Use Bootstrap 3 Glyphicons</h1>
+            <p>Glyphicons are great! They're one of my favourite additions to native Bootstrap 3. I've been working with these glyphs quite a lot, so I thought I'd make a cheat sheet for myself.</p>
+            <h2>Cheat sheet?</h2>
+            <p>Yeah! To use this cheat sheet, simply find the glyphicon you want to use and click the copy button. This will add the complete HTML code for that glyphicon (span tag and all) to your clipboard, ready to paste into your web development tool of choice.</p>
+            <h2>What are Glyphicons?</h2>
+            <p>Glyphicons are basically little symbols, icons, or pictograms (whatever you prefer to call them) that you can use in a webpage. They're implemented in Bootstrap as an icon font — a custom font that contains these glyphs instead of letters.</p>
+            <p>If you're creating a site based on Bootstrap 3.3.2, you already have access to all 260 glyphs in the Glyphicons 1.9 set.</p>
+            <p>Don't know how to set up Bootstrap? Go check out the offical <a href="http://getbootstrap.com/getting-started/" target="_blank">getting started guide</a>, or simply just include these two lines in your <code>&lt;head&gt;</code> tag:</p>
+                        </div>
+                        <div className="col-md-4">
+                            <h1>About me</h1>
+                            <p><a href="http://twitter.com/jamescroft"><img src="images/james.jpg" alt="James Croft" className="img-circle" width="140" height="140" /></a></p>
+                            <p>My name is <a href="http://twitter.com/jamescroft">James Croft</a>. I'm a web developer from Brisbane, Australia.</p>
+                            <p>If you found this page useful, consider buying <a href="http://bootstrapcheatsheets.com/shop" className="muted">a Bootstrap Cheat Sheet poster</a> for your wall!</p>
+                        </div>
+                    </div>
+                 </div>
+          );
+          }
+        }); 
+
+
              
         var Cheatsheet = React.createClass({        
             loadCommentsFromServer: function() {
@@ -68,9 +133,14 @@
               },
               render: function() {
                 return (
+                  
                   <div className="col-md-12">
-                    <h1 className="text-center">Bootstrap Glyphicons v3.2.2</h1>
-                    <IconList data={this.state.data} />     
+                  <NavBarCheat />
+                    <div className="ad-break-top"></div>
+                    <h1 className="cheatsheet-title">Bootstrap Glyphicons <span className="cheatsheet-version"> v3.2.2</span></h1>
+                    
+                    <IconList data={this.state.data} />   
+                    <AboutCheatsheet />
                   </div>
                 );
               }
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,10 +30,30 @@ $ ("a.copy-unicode").on ("mouseover", function() {
 $ ("a.copy-css").on ("mouseover", function() {
     glyphStore = $(this).parents("div.icon").children("span.glyphicon");
     glyphClipboard = "content:\"\\" + glyphStore.text() + "\";";
-    $(this).html($("<code/>").text(glyphClipboard));
+    $(this).html($("<code/>").text("" + glyphClipboard + ""));
 });
-
     
+//Create rows: 10 icons per row
+
+	var glyphCount = $("div.icon").length;	
+	var rowSize = 12;
+	for (var c = 0; c <= glyphCount; c += 24) {
+		$("div.icon").slice(c, c + rowSize).wrapAll("<div class='row'></div>");
+	};
+	for (var c = 12; c <= glyphCount; c += 24) {
+		$("div.icon").slice(c, c + rowSize).wrapAll("<div class='row row-alt'></div>");
+	};
+	$("div.iconList .row:first").attr("id", "first-row");
+
+//Add 3 Adsense banners    
+
+    $("div.ad-break-top").html("<div class=\"text-center ad-break-top\"><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><!-- Glyphicons 1st --><ins class=\"adsbygoogle\"style=\"display:inline-block;width:728px;height:90px\"data-ad-client=\"ca-pub-8168184751026221\"data-ad-slot=\"9009612991\"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>");
+      
+    $("div.iconList .row:nth-child(3)").attr("id", "third-row");
+    $("<div class='text-center ad-break'><script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script><!-- Glyphicons 2nd --><ins class='adsbygoogle' style='display:inline-block;width:728px;height:90px' data-ad-client='ca-pub-8168184751026221' data-ad-slot='1486346194'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>").insertAfter("#third-row");                           
+    $("div.iconList .row:nth-child(6)").attr("id", "sixth-row");
+    $("<div class='text-center ad-break'><script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script><!-- Glyphicons 3rd --><ins class='adsbygoogle' style='display:inline-block;width:728px;height:90px' data-ad-client='ca-pub-8168184751026221' data-ad-slot='2963079391'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>").insertAfter("#sixth-row");
+        
     
 
     
@@ -43,6 +63,10 @@ $ ("a.copy-html").on ("mouseout", function() {
     
 $ ("a.copy-unicode").on ("mouseout", function() {
     $(this).html("<span class=\"glyphicon glyphicon-screenshot\"></span> HTML Entity");
+});
+    
+$ ("a.copy-css").on ("mouseout", function() {
+    $(this).html("<span class=\"glyphicon glyphicon-globe\"></span> CSS Rule");
 });
 
 },500);
